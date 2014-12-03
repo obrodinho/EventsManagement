497b2ece9fc69574d938a9b3cd45a9c128855d0f
Mario Antonio Bortoli Filho <Mario@marios-mbp.home>
2014-12-02 23:55:34 -0300

    Faltou esse arquivo no ultimo commit

--- src/main/java/org/consultjr/mvc/service/AuthenticationInterceptor.java
+++ src/main/java/org/consultjr/mvc/service/AuthenticationInterceptor.java
@@ -16,27 +16,27 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  */
 public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
     private RequestAnalyser rqAnalyser;
-    @Override
-    public boolean preHandle(HttpServletRequest request,HttpServletResponse response,Object controller) throws Exception {
-        String uri = request.getRequestURI();
-        User user = (User) request.getSession().getAttribute("usuarioLogado");
-        //Se Usuario esta logado
-        if(user != null) {
-            //Verifica se ele tem permissao para acessar a pagina
-            if(rqAnalyser.checkPermission(user, request)){
-                return true;
-            } else {
-                //se nao tiver manda de volta pra tela inicial dele
-                response.sendRedirect("/menu");
-                return false;
-            }
-        } else {
-            //se nao ta logado volta pra tela de login
-            response.sendRedirect("/loginForm");
-            return false;
-        }
-        
-        
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request,HttpServletResponse response,Object controller) throws Exception {
+//        String uri = request.getRequestURI();
+//        User user = (User) request.getSession().getAttribute("usuarioLogado");
+//        Se Usuario esta logado
+//        if(user != null) {
+//            Verifica se ele tem permissao para acessar a pagina
+//            if(rqAnalyser.checkPermission(user, request)){
+//                return true;
+//            } else {
+//                se nao tiver manda de volta pra tela inicial dele
+//                response.sendRedirect("/menu");
+//                return false;
+//            }
+//        } else {
+//            se nao ta logado volta pra tela de login
+//            response.sendRedirect("/loginForm");
+//            return false;
+//        }
+//        
+//        
+//    }
     
 }
