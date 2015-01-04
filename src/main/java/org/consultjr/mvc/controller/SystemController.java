58a082c7eb802ab490d3c347de0e96bc1e356fc9
Rafael Guimarães <boss@rguimaraens.com>
2015-01-04 18:29:41 -0300

    Processo de instalação inciado. Intervenção e questionamento sobre a instalação deve ser mais assertivo e usar AOP ou um Interceptor. Ver comentários nos srquivos desse commit

--- src/main/java/org/consultjr/mvc/controller/SystemController.java
+++ src/main/java/org/consultjr/mvc/controller/SystemController.java
@@ -7,7 +7,6 @@ package org.consultjr.mvc.controller;
 
 import java.util.Date;
 import org.consultjr.mvc.core.base.ApplicationController;
-import org.consultjr.mvc.core.components.ApplicationUtils;
 import org.consultjr.mvc.model.Activity;
 import org.consultjr.mvc.model.ActivityType;
 import org.consultjr.mvc.model.Classes;
@@ -30,6 +29,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -61,16 +62,9 @@ public class SystemController extends ApplicationController {
     @Autowired
     private SubscriptionProfileService subsProfileService;
 
-    public SystemConfigService getSystemConfigService() {
-        return systemConfigService;
-    }
-
-    public void setSystemConfigService(SystemConfigService systemConfigService) {
-        this.systemConfigService = systemConfigService;
-    }
-
+    @RequestMapping("")
     public ModelAndView index() {
-        return new ModelAndView("redirect:/System/admin");
+        return new ModelAndView("redirect:/System/settings");
     }
 
     @RequestMapping("settings")
@@ -80,28 +74,113 @@ public class SystemController extends ApplicationController {
         return sysView;
     }
 
-    @RequestMapping("/install")
+    @RequestMapping(value = "install", method = RequestMethod.GET)
     public ModelAndView install() {
-        ModelAndView sysView = new ModelAndView("forward:/login");
+        ModelAndView sysView = new ModelAndView("System/install");
 
-        if (systemConfigService.getConfigByKey("_installed") == null) {
+        if (null != systemConfigService.getConfigByKey("_installed")) {
+            sysView.addObject("message", "The system already was installed.");
+            sysView.setViewName("forward:/login");
+            return sysView;
+        }
+
+        sysView.addObject("install", new Object());
+        return sysView;
+    }
+
+    @RequestMapping(value = "install", method = RequestMethod.POST)
+    public ModelAndView setup(
+            @RequestParam(value = "installKey", required = true) String installKey,
+            @RequestParam(value = "adminUsername", required = false) String adminUsername,
+            @RequestParam(value = "adminPassword", required = false) String adminPassword
+    ) {
+
+        ModelAndView setupView = new ModelAndView("forward:/");
+        /* TODO make it on a hash
+         KEY COULD BE LIKE XXXX-XXXX-9999-9999
+         Where:  XXXX-XXXX = 2 hex groups from a hash string
+         9999-9999 = 2 four-digit arbritary numbers (until now)
+         ------------------------------------------------------------------------
+         * org.consultjr.EventsManagement|productType=multiEvents|generated=2015-01-04|valid=2015-12-31
+         ADLER32: 88B520F8 => 88B5-20F8
+         ------------------------------------------------------------------------
+         * org.consultjr.EventsManagement|productType=singleEvent|generated=2015-01-04|valid=2015-12-31
+         ADLER32: 83FF20DC => 83FF-20DC
+         ------------------------------------------------------------------------
+         The very first group is enough to realize wich product will be set.
+         We could "voyage" on this idea and the key possibly could hold great info about system's variance.
+         Each group could hold 16 bit of information. So, up to 16 variance definitions, such like:
+         General variances:
+         Language;
+         Timezone;
+         Charset;
+         Database schema;
+         Default Template;
+         ...
+         Product-specific variances;
+         Enviromental variances:
+         Validation servers;
+         Update-check time;
+         ...
+         And so on...
+         Possibilities are enormous.
+         Our limits is only ours.
+         */
+        String productType = null;
+        getLogger().info(installKey);
+        getLogger().info(String.valueOf(installKey.length()));
+
+        if (19 == installKey.length()) {
+            String[] keyParts = installKey.split("-");
+            if (null != keyParts[0]) {
+                switch (keyParts[0]) {
+                    case "88B5":
+                        productType = "multiEvents";
+                        break;
+                    case "83FF":
+                        productType = "singleEvent";
+                        break;
+                }
+            }
+        } else {
+            setupView.addObject("message", "Invalid KEY. Please inform us a valid one.");
+            setupView.setViewName("System/install");
+            return setupView;
+        }
+
+        if (null != productType && null == systemConfigService.getConfigByKey("_installed")) {
+            /*
+             Here we must test if the database is reachable, and decide to create it, or not.
+             The database must be create on system instalation
+             */
 
             SystemProfile adminProfile = new SystemProfile("admin", "Administrador do Sistema");
             spService.addSystemProfile(adminProfile);
 
             SystemProfile userProfile = new SystemProfile("client", "Usuario Final");
             spService.addSystemProfile(userProfile);
-            
+
             SystemProfile guestProfile = new SystemProfile("guest", "Convidado");
             spService.addSystemProfile(guestProfile);
-            
-            User defaultUser = new User("Administrador", "do Sistema", "admin", "admin@LPS");
+
+            /*
+             TODO we must test the strongness and validity of username and password.
+             */
+            adminUsername = (!adminUsername.isEmpty() ? adminUsername : "admin");
+            adminPassword = (!adminPassword.isEmpty() ? adminPassword : "admin@LPS");
+            User defaultUser = new User(
+                    "Administrador",
+                    "do Sistema",
+                    adminUsername,
+                    adminPassword
+            );
+
             userService.addUser(defaultUser);
 
-            UserSystemProfile usp = new UserSystemProfile(userService.getUserByUsername("admin"), spService.getSystemProfileByShortname("admin"));
+            UserSystemProfile usp = new UserSystemProfile(userService.getUserByUsername(adminUsername), spService.getSystemProfileByShortname("admin"));
             uspService.addUserSystemProfile(usp);
 
-            Event evt = new Event("Evento Padrão", "Padrão", userService.getUserByUsername("admin"), new Date(), new Date());
+            Event evt = new Event("Evento Padrão", "Padrão", userService.getUserByUsername(adminUsername), new Date(), new Date());
             evtService.addEvent(evt);
 
             actTypeService.addActivityType(new ActivityType("Default", "default", "Example Description"));
@@ -120,15 +199,17 @@ public class SystemController extends ApplicationController {
 
             systemConfigService.addConfig(new SystemConfig("_installed", "yes"));
             systemConfigService.addConfig(new SystemConfig("_configuredAt", new Date().toString()));
-            systemConfigService.addConfig(new SystemConfig("_productType", "monoevento"));
 
-            sysView.addObject("message", "Initial database objects has been created.");
+            systemConfigService.addConfig(new SystemConfig("_productType", productType));
+
+            setupView.addObject("message", "Initial database objects has been created.");
 
         } else {
-            sysView.addObject("message", "Initial database objects has already been created.");
+            setupView.addObject("message", "Installation failed. Check your product KEY or try contacting support.");
+            setupView.setViewName("forward:/");
         }
 
-        return sysView;
+        return setupView;
     }
 
 }
---
+++ src/main/webapp/views/System/install.jsp
@@ -0,0 +1,63 @@
+<%-- 
+    Document   : install
+    Created on : 03/01/2015, 16:44:10
+    Author     : Rafael
+--%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<%@taglib uri='http://java.sun.com/jsp/jstl/core' prefix='c' %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@include file="../../templates/header.jspf" %>
+
+<div class="page-header">
+    <h2>Hey, let's begin!</h2>
+</div>
+
+<div class="jumbotron">
+    <p>You are getting on an upgradable software. 
+        If you got a trial or an limited product key, don't be affraid. 
+        You won't lose anything on features upgrade. Contact us and get newer software and upgrades.</p>
+</div>
+<form:form method="POST" action="${pageContext.request.contextPath}/System/install">
+    <fieldset>
+        <legend>
+            Product KEY
+        </legend>
+
+        <label>Please, inform us your KEY.</label>
+        <div class="form-group-hg">
+            <input autocomplete="on" maxlength="19" pattern="[0-9A-Z]{4}-[0-9A-Z]{4}-[0-9]{4}-[0-9]{4}" name="installKey" id="installKey" class="form-control" placeholder="A1B2-C3D4-1234-5678" /> 
+            <p class="help-block">
+                Inform us your <abbr title="It's something like 'A1B2-C3D4-1234-5678'">KEY</abbr> to install your software.
+            </p>
+        </div>
+
+    </fieldset>
+
+    <fieldset>
+        <legend>
+            Administrative user data
+        </legend>
+
+        <label>Do you wanna define an administrative user username?</label>
+        <div class="form-group-hg">
+            <input autocomplete="off" maxlength="16" name="adminUsername" id="adminUsername" class="form-control" placeholder="admin? root? john.doe?" /> 
+            <p class="help-block">
+                Leave blank if you like to use the standard username for the adminstrative user. It's <strong>admin</strong>. We limit it to 16 characters.
+            </p>
+        </div>
+
+        <label>Do you wanna define an administrative user password?</label>
+        <div class="form-group-hg">
+            <input type="password" autocomplete="off" maxlength="32" name="adminPassword" id="adminPassword" class="form-control" /> 
+            <p class="help-block">
+                Leave blank if you like to use the standard password for the adminstrative user. It's <strong>admin@LPS</strong>. We limit it to 32 characters.
+            </p>
+        </div>
+    </fieldset>
+
+    <hr />
+    <div class="row footer">
+        <input type="submit" value="Install" class="btn btn-default btn-embossed btn-primary btn-hg" />
+    </div>
+</form:form>
+<%@include file="../../templates/footer.jspf" %>
